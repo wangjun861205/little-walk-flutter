892a6431c278daeea6848680898ff67850faeeea
wangjun861205 <444055828@qq.com>
2023-11-06 18:19:00 +0800

    checkpoint

--- lib/screens/my_dogs_list.dart
+++ lib/screens/my_dogs_list.dart
@@ -7,15 +7,12 @@ import 'package:little_walk/screens/login.dart';
 import '../models/dog.dart';
 
 class MyDogsList extends StatelessWidget {
-Future<List<Dog>> () async {
-      final authToken = await getAuthToken();
-      if (authToken == null) {
-        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-          return const LoginScreen(60);
-        }));
-      }
-      return await myDogs(authToken!);
-    }
+  Future<List<Dog>> query() async {
+    final authToken = await getAuthToken();
+    if (authToken == null) {}
+    return await myDogs(authToken!);
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
